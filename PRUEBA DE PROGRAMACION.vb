1dd2f8e785599de3a58afe500d5da75d40480460
TimoPalo12 <142957882+TimoPalo12@users.noreply.github.com>
2023-09-05 21:32:47 -0600

    Add files via upload

---
+++ PRUEBA DE PROGRAMACION.vb
@@ -0,0 +1,18 @@
+﻿Public Class Form6
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim a, b, resultado As Double
+        a = TextBox1.Text
+        b = TextBox2.Text
+        resultado = a / b
+        TextBox3.Text = resultado
+        If TextBox1.Text = 0 Then
+            TextBox3.Text = "Error"
+        ElseIf TextBox2.Text = 0 Then
+            TextBox3.Text = "Error"
+        End If
+    End Sub
+
+    Private Sub Form6_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
